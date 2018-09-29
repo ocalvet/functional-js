00885f4cb566de7f724e107145406f46b7d989ca
Ovidio Calvet <calveto@gmail.com>
2018-09-28 21:54:35 -0400

    Add lodash and use the curry function from it

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const _ = require('lodash');
+
 const testData = [1, 2, 3, 10, 5, 6, 22, 10, 12];
 
 isNotA10 = number => number !== 10;
@@ -5,3 +7,12 @@ isNotA10 = number => number !== 10;
 testData.forEach(number => {
   console.log(isNotA10(number) ? 'Not a ten' : 'Ten');
 });
+
+const get = _.curry((property, obj) => obj[property]);
+
+const data = {
+  test: '1234'
+}
+
+console.log(get('test')(data));
+
